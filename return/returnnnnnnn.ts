0c4cd1e94417cd08120bfedafe552a9abf0a0da9
Octopus142 <blackfairyforma726@gmail.com>
2024-10-11 16:23:25 +0800

    Update returnnnnnnn.ts

--- return/returnnnnnnn.ts
+++ return/returnnnnnnn.ts
@@ -2,11 +2,11 @@ let t11:any;
 let t0:any;
 void function t11t0(){
     void t11[
-       'w ^ Math.exp(NaN + t5)' + 'x ^ Math.log(NaN - t6)',
-       'y ^ Math.exp(NaN - t5)' + 'z ^ Math.log(NaN + t6)'
+       'w ^ Math.exp(NaN + t11)' + 'x ^ Math.log(NaN - t0)',
+       'y ^ Math.exp(NaN - t11)' + 'z ^ Math.log(NaN + t0)'
     ]
     void t0[
-       'w ^ Math.exp(0 + t5)' + 'x ^ Math.log(0 - t6)',
-       'y ^ Math.exp(0 - t5)' + 'z ^ Math.log(0 + t6)'
+       'w ^ Math.exp(0 + t11)' + 'x ^ Math.log(0 - t0)',
+       'y ^ Math.exp(0 - t11)' + 'z ^ Math.log(0 + t0)'
     ]
 }
