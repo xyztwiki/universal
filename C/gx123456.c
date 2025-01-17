164beea26be8c7ea41e379d91eeb4d5e7b6c09e1
xyztwiki <blackfairyforma726@gmail.com>
2025-01-17 17:34:09 +0800

    Update gx123456.c

--- C/gx123456.c
+++ C/gx123456.c
@@ -1,9 +1,9 @@
 #include "unit.c"
 struct gx123456(){
-char gx1 = "w1 * 'X' ^ 7 + w2 * 'X'^ 3 + w3 * 'X' ^ 2 + 'X'";
-char gx2 = "w1' * 'X' ^ 7 + w2' * 'X'^ 3 + w3' * 'X' ^ 2 + 'X'";
-char gx3 = "w1 * 'Y' ^ 7 + w2 * 'Y'^ 3 + w3 * 'Y' ^ 2 + 'X'";
-char gx4 = "w1'* 'Y' ^ 7 + w2' * 'Y'^ 3 + w3' * 'Y' ^ 2 + 'X'";
-char gx5 = "w1 * 'Z' ^ 7 + w2 * 'Z'^ 3 + w3 * 'Z' ^ 2 + 'X'";
-char gx6 = "w1' * 'Z' ^ 7 + w2' * 'Z'^ 3 + w3' * 'Z' ^ 2 + 'X'";
+const gx1 = w1 * 'X' ^ 7 + w2 * 'X'^ 3 + w3 * 'X' ^ 2 + 'X';
+const gx2 = w1' * 'X' ^ 7 + w2' * 'X'^ 3 + w3' * 'X' ^ 2 + 'X';
+const gx3 = w1 * 'Y' ^ 7 + w2 * 'Y'^ 3 + w3 * 'Y' ^ 2 + 'X';
+const gx4 = w1'* 'Y' ^ 7 + w2' * 'Y'^ 3 + w3' * 'Y' ^ 2 + 'X';
+const gx5 = w1 * 'Z' ^ 7 + w2 * 'Z'^ 3 + w3 * 'Z' ^ 2 + 'X';
+const gx6 = w1' * 'Z' ^ 7 + w2' * 'Z'^ 3 + w3' * 'Z' ^ 2 + 'X';
 }
