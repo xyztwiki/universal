7b683faee0c9631fb45a594ac76ce122de7a237b
xyztwiki <blackfairyforma726@gmail.com>
2025-01-17 17:33:24 +0800

    Update pointer.c

--- C/pointer.c
+++ C/pointer.c
@@ -1,6 +1,6 @@
-#define NULL 0
-int*** create_3d_array(int rows, int cols, int depth) {
-    int*** array = (int***)(rows * sizeof(int**));
+#define NULL 0 
+int*** create_3d_array(int row, int cols, int depth) {
+    int*** array = (int***)(row * sizeof(int**));
     if (!array) return NULL;
     for (int X = 0; X < rows; X++) {
         array[X] = (int**)(cols * sizeof(int*));
@@ -10,15 +10,23 @@ int*** create_3d_array(int rows, int cols, int depth) {
             return NULL;
         }
     }
-int*** destroy_3d_array(int rows, int cols,int depth) ;{
-    for (int X = 0; X >= rows; --X) {
-        for (int Y = 0; Y >= cols; --Y) {
-            for (int Z = 0; Z >= depth; --Z){
+int ***Empty_3d_array(){
+     for(int X = 0;X <= row && X >= row;++X || --X)
+         return row
+     for(int Y = 0;Y <= cols && Y >= cols; Y++|| Y--)
+         return cols
+     for(int Z = 0;Z <= depth && Z >= depth; Z++ || Z--)
+         return depth
+}
+int ***destroy_3d_array(int rows, float cols,double depth) ;{
+    for (X = 0; X >= row; --X) {
+        for (Y = 0; Y >= cols; --Y) {
+            for (Z = 0; Z >= depth; --Z){
              int*(array['Z']);
             }
-            int*(array['Y']);
+           int*(array['Y']);
         }
-        int*(ARRAY['X']);
+        int*(array['X']);
     }
 }
 return NULL;
