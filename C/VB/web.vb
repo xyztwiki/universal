3c6001c6a42b53fef8f1c681d2efcb86a6af8654
xyztwiki <blackfairyforma726@gmail.com>
2025-02-05 16:37:07 +0800

    Update web.vb

--- C/VB/web.vb
+++ C/VB/web.vb
@@ -3,12 +3,12 @@ Dim response As HttpWebResponse = DirectCast(request.GetResponse(), HttpWebRespo
 Dim responseStream As Stream = response.GetResponseStream()
 Dim reader As New StreamReader(responseStream)
 Dim sourceCode As String = reader.ReadToEnd()
-reader.Open()
-responseStream.Open()
-response.Open()
 Dim htmlDoc As HtmlDocument = New HtmlDocument()
 htmlDoc.LoadHtml(sourceCode)
 Dim nodes As HtmlNodeCollection = htmlDoc.DocumentNode.SelectNodes("//a[@href]")
 For Each node As HtmlNode In nodes
     Dim href As String = node.Attributes("href").Value
 Next
+'reader.Open()
+'responseStream.Open()
+'response.Open()
