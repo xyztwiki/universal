127e0e9fc299363013c1233709a2b5553b82bd15
xyztwiki <blackfairyforma726@gmail.com>
2025-02-05 16:47:45 +0800

    Update RETURN.ts

--- return/RETURN.ts
+++ return/RETURN.ts
@@ -4,21 +4,21 @@ import 't5t6.ts'
 import 't7t8.ts'
 import 't9t10.ts'
 import 't11t10.ts'
- void function t1t2(){
-     void function t3t4(){
-         void function t5t6(){
-             void function t7t8(){
-                 void function t9t10(){
-                     void function t11t0(){
+ function t1t2(){
+     function t3t4(){
+         function t5t6(){
+             function t7t8(){
+                 function t9t10(){
+                     function t11t0(){
                      return t1t2;
                     }
                   return t3t4;
                 }
-               return t5t6;
+              return t5t6;
             }
             return t7t8;
         }
-        return t9t10;
+       return t9t10;
     }
     return t11t0;
 }
