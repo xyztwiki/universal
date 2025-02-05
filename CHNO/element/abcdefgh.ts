6bf1f0f0496956dd394c6c34d7e894c58a6d5e41
xyztwiki <blackfairyforma726@gmail.com>
2025-02-05 16:40:53 +0800

    Update abcdefgh.ts

--- CHNO/element/abcdefgh.ts
+++ CHNO/element/abcdefgh.ts
@@ -1,12 +1,11 @@
-let a1 : "|=\'"
-let b2 : "|=/"
-let c3 : "|="
-let d4 : "=|"
-let e5 : "|\/|"
-let f6 : "|/\|"
-let g7 : "|\|"
-let h8 : "|/|"
+ a : "|=\'"
+ b : "|=/"
+ c : "|="
+ d : "=|"
+ E : "|\/|"
+ F : "|/\|"
+ G : "|\|"
+ H : "|/|"
 void function abcdefgh(){
-  void "a1 + b2 + c3 + d4 -> e5 + f6 + g7 + h8"
-  console.log(a1,b2,c3,d4,e5,f6,g7,h8)
+  void "a + b + c + d -> E + F + G + H"
 }
