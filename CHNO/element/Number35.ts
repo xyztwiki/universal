ba13a7f38099fcb6e368df203e9f818348e8263a
xyztwiki <blackfairyforma726@gmail.com>
2025-02-05 16:50:01 +0800

    Update Number35.ts

--- CHNO/element/Number35.ts
+++ CHNO/element/Number35.ts
@@ -1,16 +1,3 @@
-void function number() {
-  const one: number = 1
-  const two: number = 2
-  const three: number = 3
-  const four: number = 4
-  const five: number = 5
-  const six: number = 6
-  const seven: number = 7
-  const eight: number = 8
-  const nine: number = 9
-  const zero: number = 0
-  console.log(one,two,three,four,five,six,seven,six,eight,nine,zero)
-}
   const one =
        [[0, 1, 0],
         [0, 1, 0],
