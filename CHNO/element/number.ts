f802db4513718295f85d91861d9c88229282aff2
xyztwiki <blackfairyforma726@gmail.com>
2025-02-05 16:50:52 +0800

    Create number.ts

---
+++ CHNO/element/number.ts
@@ -0,0 +1,13 @@
+function number() {
+  type one = 1;
+  type two = 2;
+  type three = 3;
+  type four = 4;
+  type five = 5;
+  type six = 6;
+  type seven = 7;
+  type eight = 8;
+  type nine = 9;
+  type zero = 0;
+  console.log(one,two,three,four,five,six,seven,six,eight,nine,zero)
+}
