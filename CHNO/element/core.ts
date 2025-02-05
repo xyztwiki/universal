cf033d41e8353b204c46220467bc0186694cc6cb
xyztwiki <blackfairyforma726@gmail.com>
2025-02-05 16:41:27 +0800

    Update core.ts

--- CHNO/element/core.ts
+++ CHNO/element/core.ts
@@ -1,8 +1,7 @@
-let H2 : "2|_|1|-|"
-let H3 : "3|_|1|-|"
-let He : "4|_|2|-|"
-let H1 : "1|\|0|-|"
+ H2 : "2|_|1|-|"
+ H3 : "3|_|1|-|"
+ He : "4|_|2|-|"
+ H1 : "1|_|0|-|"
 void function core(){
   return "H2 + H3 -> He + H1"
 }
-console.log('H2','H3','He','H1')
