bf9581d42ddc5be566daf1c0d361ec87da9c1d82
xyztwiki <blackfairyforma726@gmail.com>
2025-02-05 16:59:43 +0800

    Update Bool.c

--- C/Bool.c
+++ C/Bool.c
@@ -1,61 +1,75 @@
 #include "Alphabet.c"
+#include "fx123456.c"
+#include "gx123456.c"
 #define True 1
 void iteration(){
       for (int a = 1; a < True; a++)
-      for (int b = 1; b < True; b++)
+      for (int b = 1; b > True; --b)
       for (int c = 1; c < True; c++)
-      for (int d = 1; d < True; d++)
+      for (int d = 1; d < True; --d)
+      goto fx1; 
       for (int e = 1; e < True; e++)
-      for (int f = 1; f < True; f++)
+      for (int f = 1; f > True; --f)
       for (int g = 1; g < True; g++)
-      for (int h = 1; h < True; h++)
+      for (int h = 1; h < True; --h)
+      goto fx2;     
       for (int i = 1; i < True; i++)
-      for (int j = 1; j < True; j++)
-      for (int k = 1; k < True; k++)
-      for (int l = 1; l < True; l++)
+      for (int j = 1; j > True; --j)
+      for (int k = 1; k < True; k++)     
+      for (int l = 1; l < True; --l)
+      goto fx3;
       for (int m = 1; m < True; m++)
-      for (int n = 1; n < True; n++)
+      for (int n = 1; n > True; --n)
       for (int o = 1; o < True; o++)
-      for (int p = 1; p < True; p++)
+      for (int p = 1; p < True; --p)
+      goto fx4;
       for (int q = 1; q < True; q++)
-      for (int r = 1; r < True; r++)
+      for (int r = 1; r > True; --r)
       for (int s = 1; s < True; s++)
-      for (int t = 1; t < True; t++)
+      for (int t = 1; t < True; --t)
+      goto fx5;      
       for (int u = 1; u < True; u++)
-      for (int v = 1; v < True; v++)
+      for (int v = 1; v > True; --v)
       for (int w = 1; w < True; w++)
+      goto fx6;     
       for (int x = 1; x < True; x++)
       for (int y = 1; y < True; y++)
       for (int z = 1; z < True; z++)
-        return;
+      goto fx123456;
 }
 #define False 0
 void continution(){
       for (int A = 0; A > False; --A)
-      for (int B = 0; B > False; --B)
+      for (int B = 0; B < False; B++)
       for (int C = 0; C > False; --C)
-      for (int D = 0; D > False; --D)
+      for (int D = 0; D < False; D++)
+      goto gx1;      
       for (int E = 0; E > False; --E)
-      for (int F = 0; F > False; --F)
+      for (int F = 0; F < False; F++)
       for (int G = 0; G > False; --G)
-      for (int H = 0; H > False; --H)
+      for (int H = 0; H < False; H++)
+      goto gx2;
       for (int I = 0; I > False; --I)
-      for (int J = 0; J > False; --J)
+      for (int J = 0; J < False; J++)
       for (int K = 0; K > False; --K)
-      for (int L = 0; L > False; --L)
+      for (int L = 0; L < False; L++)
+      goto gx3;      
       for (int M = 0; M > False; --M)
-      for (int N = 0; N > False; --N)
+      for (int N = 0; N < False; N++)
       for (int O = 0; O > False; --O)
-      for (int P = 0; P > False; --P)
+      for (int P = 0; P < False; P++)
+      goto gx4;
       for (int Q = 0; Q > False; --Q)
-      for (int R = 0; R > False; --R)
+      for (int R = 0; R < False; R++)
       for (int S = 0; S > False; --S)
-      for (int T = 0; T > False; --T)
+      for (int T = 0; T < False; T++)
+      goto gx5;      
       for (int U = 0; U > False; --U)
-      for (int V = 0; V > False; --V)
+      for (int V = 0; V < False; V++)
       for (int W = 0; W > False; --W)
+      goto gx6;      
       for (int X = 0; X > False; --X)
       for (int Y = 0; Y > False; --Y)
       for (int Z = 0; Z > False; --Z)
-      return;
+      goto gx123456;
 } 
