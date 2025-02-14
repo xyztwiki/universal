fd65b3018caef4d08d0b30268b575f2326fb0a53
xyztwiki <blackfairyforma726@gmail.com>
2025-02-14 16:08:31 +0800

    Update and rename gx123456.c to gw123456.c

---
+++ C/gw123456.c
@@ -0,0 +1,9 @@
+#include "unit.c"
+struct gw123456(){
+const gw1 = w1 * 'X' ^ 7 + w2 * 'X'^ 3 + w3 * 'X' ^ 2 + 'X';
+const gw2 = w1' * 'X' ^ 7 + w2' * 'X'^ 3 + w3' * 'X' ^ 2 + 'X';
+const gw3 = w1 * 'Y' ^ 7 + w2 * 'Y'^ 3 + w3 * 'Y' ^ 2 + 'X';
+const gw4 = w1'* 'Y' ^ 7 + w2' * 'Y'^ 3 + w3' * 'Y' ^ 2 + 'X';
+const gw5 = w1 * 'Z' ^ 7 + w2 * 'Z'^ 3 + w3 * 'Z' ^ 2 + 'X';
+const gw6 = w1' * 'Z' ^ 7 + w2' * 'Z'^ 3 + w3' * 'Z' ^ 2 + 'X';
+}
