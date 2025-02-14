955b76ff2dbeae589d55a3cf1d1ecaf5db24cec6
xyztwiki <blackfairyforma726@gmail.com>
2025-02-14 16:08:09 +0800

    Update and rename fx123456.c to fw123456.c

---
+++ C/fw123456.c
@@ -0,0 +1,12 @@
+#include "unit.c"
+int unit(){
+   void activate();{
+     const fw =  1 / 1 + exp(-1);
+}
+      const  fw1 = w1x1 + w2x2 + w3x3;
+      const  fw2 = "w1'x1 + w2'x2 + w3'x3";
+      const  fw3 = w1y1 + w2y2 + w3y3;
+      const  fw4 = "w1'y1 + w2'y2 + w3'y3";
+      const  fw5 = w1z1 + w2z2 + w3z3;
+      const  fw6 = "w1'z1 + w2'z2 + w3'z3";
+}
